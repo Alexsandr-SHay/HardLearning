0099bea3a2e74559a97a0b458aaa728c7d157149
Alexand Shaymardanov <sovereing92@mail.ru>
2024-12-03 21:22:51 +0600

    Исправленное второе задание

--- script.js
+++ script.js
@@ -2,17 +2,9 @@ let num = 266219;
 num = num.toString();
 let result = 1;
 for (let i = 0; i <= num.length - 1; i++) {
-  result *= number(num[i]);
+  result *= Number(num[i]);
 }
 console.log(result);
 
 console.log(result ** 3);
 console.log((result ** 3).toString().substring(0, 2));
-
-console.log(
-  /*Рабочий метод только для первой части задания*/
-  num
-    .toString()
-    .split("")
-    .reduce((acc, item) => acc * item, 1)
-);
