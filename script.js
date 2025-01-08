2ff73c11b2f2b27751835736dd7ab8ee96287d29
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-09 00:06:48 +0600

    Урок 19.2 функция дебонс

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const inputText = document.querySelector(".input-text");
+const textPar = document.querySelector(".text-p");
+
+function debounce(callback, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
+  };
+}
+
+const text = () => {
+  textPar.textContent = "Текст в поле ввода " + inputText.value;
+};
+
+inputText.addEventListener("keyup", debounce(text, 500));
