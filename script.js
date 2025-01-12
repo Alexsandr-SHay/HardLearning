54629c03697ca8ac4f5f3d1be08b8ff59b0e67fb
Alexand Shaymardanov <sovereing92@mail.ru>
2025-01-12 18:48:48 +0600

    Урок19.3 исправленный

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-'use strict'
+"use strict";
 
-const square = document.querySelector('.square');
-const buttonStart = document.querySelector('.start');
-const buttonStop = document.querySelector('.stop');
-const buttonReset = document.querySelector('.reset');
+const square = document.querySelector(".square");
+const buttonStart = document.querySelector(".start");
+const buttonStop = document.querySelector(".stop");
+const buttonReset = document.querySelector(".reset");
 let requestId;
 let valueSquare = 0;
 
 const request = () => {
-    requestId = requestAnimationFrame(() => {
-        valueSquare = parseFloat(valueSquare) + 10 + "px"
-        square.style.left = valueSquare
-        console.log(valueSquare)
-        request();
-    })
-}
+  requestId = requestAnimationFrame(() => {
+    valueSquare = parseFloat(valueSquare) + 10 + "px";
+    square.style.left = valueSquare;
+    console.log(valueSquare);
+    request();
+  });
+};
 
 buttonStart.addEventListener("click", () => {
-    buttonStart.style.display = "none"
-    buttonStop.style.display = "block"
-    request()
-
-})
+  buttonStart.style.display = "none";
+  buttonStop.style.display = "block";
+  request();
+});
 
 buttonStop.addEventListener("click", () => {
-    buttonStart.style.display = "block"
-    buttonStop.style.display = "none"
-    cancelAnimationFrame(requestId);
-})
+  buttonStart.style.display = "block";
+  buttonStop.style.display = "none";
+  cancelAnimationFrame(requestId);
+});
 
-buttonReset.addEventListener('click', () => {
-    square.style.left = 10 + "px"
-})
-
-
-
-
+buttonReset.addEventListener("click", () => {
+  valueSquare = 0;
+  square.style.left = 10 + "px";
+});
